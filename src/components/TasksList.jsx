54e720ecf2d917504d911b53103e47f4a6c503dc
Maryia Hutkouskaya <gutkovskaya18@gmail.com>
2025-06-23 22:52:05 +0300

    incr 1

---
+++ src/components/TasksList.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+
+const TasksList = () => {
+    const [tasks, setTasks] = useState([
+        'Купить хлеб',
+        'Стать фронтенд разработчиком',
+        'Выучить Typescript'
+    ]);
+    const [newTask, setNewTask] = useState('');
+
+    const onChangeInput = (e) => {
+        setNewTask(e.target.value);
+    };
+
+    const onAddTask = () => {
+        setTasks((prevTasks) => [...prevTasks, newTask]);
+        setNewTask('');
+    };
+
+    const onDeleteLastTask = () => {
+        //setTasks((prevTasks) => prevTasks.filter((task, i, arr) => i !== arr.length - 1));
+        setTasks((prevTasks) => prevTasks.slice(0, -1));
+    };
+
+    return (
+        <>
+            <h3>Список задач</h3>
+            <ul>
+                {tasks.map((task) => (
+                    <li>{task}</li>
+                ))}
+            </ul>
+
+            <input value={newTask} onChange={(e) => onChangeInput(e)}></input>
+            <button onClick={onAddTask}>Добавить задачу</button>
+            <button onClick={onDeleteLastTask}>Удалить последнюю задачу</button>
+        </>
+    );
+};
+
+export default TasksList;
---
+++ src/components/UserProfile.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+const UserProfile = () => {
+    const [user, setUser] = useState({
+        name: 'Masha',
+        age: 25,
+        isActive: true
+    });
+
+    const onChangeName = () => {
+        setUser((prevUser) => ({ ...prevUser, name: 'Dasha' }));
+    };
+
+    const onIncreaseAge = () => {
+        setUser((prevUser) => ({ ...prevUser, age: prevUser.age + 1 }));
+    };
+
+    const onToggleActiveState = () => {
+        setUser((prevUser) => ({ ...prevUser, isActive: !prevUser.isActive }));
+    };
+
+    return (
+        <>
+            <h3>Профиль пользователя</h3>
+            <div>
+                <div>Имя: {user.name}</div>
+                <div>Возраст: {user.age}</div>
+                <div>Активен: {user.isActive ? 'да' : 'нет'}</div>
+            </div>
+            <div>
+                <button onClick={onChangeName}>Сменить имя</button>
+                <button onClick={onIncreaseAge}>Увеличить возраст</button>
+                <button onClick={onToggleActiveState}>Переключить активность</button>
+            </div>
+        </>
+    );
+};
+
+export default UserProfile;
